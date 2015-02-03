5e390cdd5c7a865a28b78da547487866e617ac2b
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-02-03 17:06:54 +0000

    fix UI messages

--- plugin_ide.ui/src/melnorme/lang/ide/ui/LangUIMessages_Actual.java
+++ plugin_ide.ui/src/melnorme/lang/ide/ui/LangUIMessages_Actual.java
@@ -4,13 +4,13 @@ package melnorme.lang.ide.ui;
 public abstract class LangUIMessages_Actual {
 	
 	public static String InitializeAfterLoadJob_starter_job_name
-		= "Starting D UI Plugin initialization";
+		= "Starting Rust UI Plugin initialization";
 	public static String LangPlugin_initializing_ui
-		= "Initializing D UI Plugin";
+		= "Initializing Rust UI Plugin";
 	
 	public static String error_selectProject
 		= "Select a project.";
 	public static String error_notAValidProject
-		= "Not a valid D project.";
+		= "Not a valid Rust project.";
 	
 }
