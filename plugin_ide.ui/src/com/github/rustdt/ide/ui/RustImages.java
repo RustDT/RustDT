0fa5dc6226ca9e1e12d4347f26db73cc8c60441c
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-02-11 14:05:52 +0000

    LANG: Minor images refactoring.

--- plugin_ide.ui/src/com/github/rustdt/ide/ui/RustImages.java
+++ plugin_ide.ui/src/com/github/rustdt/ide/ui/RustImages.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2015 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
 package com.github.rustdt.ide.ui;
 
 import melnorme.lang.ide.ui.LangImages;
@@ -5,6 +15,6 @@ import melnorme.lang.ide.ui.utils.PluginImagesHelper.ImageHandle;
 
 public class RustImages extends LangImages {
 	
-	public static final ImageHandle TOOLS_CONSOLE = createManaged("", "ToolsConsole.png");
+	public static final ImageHandle TOOLS_CONSOLE = createManaged("views/ToolsConsole.16.png");
 	
 }
