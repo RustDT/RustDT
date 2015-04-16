0e2f212e611e51f0b3ddbd4e6dfdd0f506601ae6
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-04-16 11:56:26 +0100

    LANG: Add core support for CA snippets. PART1

---
+++ plugin_ide.ui/src/com/github/rustdt/ide/ui/editor/templates/RustTemplateContextType.java
@@ -0,0 +1,13 @@
+package com.github.rustdt.ide.ui.editor.templates;
+
+import melnorme.lang.ide.ui.EditorSettings_Actual;
+import _org.eclipse.jdt.internal.corext.template.java.CompilationUnitContextType;
+
+public class RustTemplateContextType extends CompilationUnitContextType {
+	
+	public static String ID = EditorSettings_Actual.TEMPLATE_CONTEXT_TYPE_ID; 
+	
+	public RustTemplateContextType() {
+	}
+	
+}
