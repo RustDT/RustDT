cb8e4249f57c3c85ec5dd03dcb42f58ee438d514
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-04-23 19:15:08 +0100

    Attempt automatic initialization of Rust GDB pretty printers. Closes #6

--- plugin_ide.core/src/com/github/rustdt/ide/core/operations/RustSDKPreferences.java
+++ plugin_ide.core/src/com/github/rustdt/ide/core/operations/RustSDKPreferences.java
@@ -2,8 +2,19 @@ package com.github.rustdt.ide.core.operations;
 
 import melnorme.lang.ide.core.bundlemodel.SDKPreferences;
 import melnorme.lang.ide.core.utils.prefs.StringPreference;
+import melnorme.lang.tooling.data.AbstractValidator.ValidationException;
+import melnorme.lang.tooling.data.LocationValidator;
+import melnorme.utilbox.misc.Location;
 
 public interface RustSDKPreferences extends SDKPreferences {
+	
+	public static class RustSDKAcessor {
+		public Location getSDKLocation() throws ValidationException {
+			return new LocationValidator("Rust installation:").getValidatedField(SDK_PATH.get());
+		}
+	}
+	
+	public static final RustSDKAcessor SDK_PATH_Acessor = new RustSDKAcessor();
 	
 	public static final StringPreference SDK_SRC_PATH = new StringPreference("sdk_src_path", "");
 	
--- plugin_ide.debug/src/com/github/rustdt/ide/debug/ui/RustDebugLaunchConfigurationDelegate.java
+++ plugin_ide.debug/src/com/github/rustdt/ide/debug/ui/RustDebugLaunchConfigurationDelegate.java
@@ -1,17 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2015 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
 package com.github.rustdt.ide.debug.ui;
 
 
+import melnorme.lang.ide.core.LangCore;
 import melnorme.lang.ide.debug.core.AbstractLangDebugLaunchConfigurationDelegate;
+import melnorme.lang.ide.debug.core.services.DebugServicesExtensions;
+import melnorme.lang.tooling.data.AbstractValidator.ValidationException;
+import melnorme.utilbox.misc.ArrayUtil;
+import melnorme.utilbox.misc.Location;
 
 import org.eclipse.cdt.dsf.gdb.launching.GdbLaunch;
+import org.eclipse.cdt.dsf.gdb.service.GDBBackend;
+import org.eclipse.cdt.dsf.mi.service.IMIBackend;
+import org.eclipse.cdt.dsf.service.DsfSession;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ISourceLocator;
+
+import com.github.rustdt.ide.core.operations.RustSDKPreferences;
 
 public class RustDebugLaunchConfigurationDelegate extends AbstractLangDebugLaunchConfigurationDelegate {
 	
 	@Override
 	protected GdbLaunch doCreateGdbLaunch(ILaunchConfiguration configuration, String mode, ISourceLocator locator) {
 		return new RustGdbLaunch(configuration, mode, locator);
+	}
+	
+	@Override
+	protected DebugServicesExtensions doCreateServicesExtensions() {
+		return new DebugServicesExtensions() {
+			@Override
+			public IMIBackend createBackendGDBService(DsfSession session, ILaunchConfiguration lc) {
+				return new GDBBackend_Rust(session, lc);
+			}
+		};
+	}
+	
+	public static class GDBBackend_Rust extends GDBBackend {
+		public GDBBackend_Rust(DsfSession session, ILaunchConfiguration lc) {
+			super(session, lc);
+		}
+		
+		@Override
+		protected String[] getGDBCommandLineArray() {
+			String[] gdbCmdLine = super.getGDBCommandLineArray();
+			Location loc = null;
+			try {
+				loc = RustSDKPreferences.SDK_PATH_Acessor.getSDKLocation().resolve_fromValid("lib/rustlib/etc/");
+			} catch (ValidationException ve) {
+				LangCore.logWarning(ve.getMessage(), ve.getCause());
+				return gdbCmdLine;
+			}
+			
+			gdbCmdLine =ArrayUtil.concat(gdbCmdLine, "-d", loc.toPathString());
+			gdbCmdLine =ArrayUtil.concat(gdbCmdLine, "-iex", "add-auto-load-safe-path " + loc.toPathString());
+//			gdbCmdLine =ArrayUtil.concat(gdbCmdLine, "-iex", "source " + 
+//					loc.resolve_fromValid("gdb_rust_pretty_printing.py").toPathString());
+			return gdbCmdLine;
+		}
 	}
 	
 }
