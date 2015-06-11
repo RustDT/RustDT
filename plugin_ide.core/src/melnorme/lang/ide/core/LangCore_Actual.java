d7cf38f2584194330d6b3c538da08dfe17b0ec88
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-06-11 11:36:15 +0100

    LANG: Separate build problems from source problems.

--- plugin_ide.core/src/melnorme/lang/ide/core/LangCore_Actual.java
+++ plugin_ide.core/src/melnorme/lang/ide/core/LangCore_Actual.java
@@ -11,7 +11,8 @@ public class LangCore_Actual {
 	public static final String NATURE_ID = PLUGIN_ID +".nature";
 	
 	public static final String BUILDER_ID = PLUGIN_ID + ".Builder";
-	public static final String BUILD_PROBLEM_ID = PLUGIN_ID + ".marker_problem";
+	public static final String BUILD_PROBLEM_ID = PLUGIN_ID + ".build_problem";
+	public static final String SOURCE_PROBLEM_ID = PLUGIN_ID + ".source_problem";
 	
 	public static AbstractToolsManager createToolManagerSingleton() {
 		return new AbstractToolsManager() { };
