bb89152fea72aaf8ed3e98f8d3db1bc53da5804c
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-08-10 17:25:27 +0100

    LANG: workspace-wide handling of build markers creation/deletion.
    Conflicts:
    	plugin_ide.ui/src/melnorme/lang/ide/ui/LangUIPlugin_Actual.java

--- plugin_ide.ui/src/melnorme/lang/ide/ui/LangUIPlugin_Actual.java
+++ plugin_ide.ui/src/melnorme/lang/ide/ui/LangUIPlugin_Actual.java
@@ -2,6 +2,7 @@ package melnorme.lang.ide.ui;
 
 import java.util.List;
 
+import melnorme.lang.ide.core.LangCore_Actual;
 import melnorme.lang.ide.ui.editor.hover.ILangEditorTextHover;
 import melnorme.lang.ide.ui.views.StructureElementLabelProvider;
 
@@ -44,6 +45,8 @@ public final class LangUIPlugin_Actual {
 	}
 	
 	/* ----------------- UI messages:  ----------------- */
+	
+	public static final String TOOLS_CONSOLE_NAME = LangCore_Actual.LANGUAGE_NAME + " build";
 	
 	public static final String DAEMON_TOOL_Name = "Racer";
 	public static final String DAEMON_TOOL_ConsoleName = "RustDT Racer log";
