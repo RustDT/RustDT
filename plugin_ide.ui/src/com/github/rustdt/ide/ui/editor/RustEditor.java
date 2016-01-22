0a9e0255a3f8882e68c904d863ccf9eb113e0e12
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-01-22 13:41:16 +0000

    Simplify outline view structure, filter some children.

--- plugin_ide.ui/src/com/github/rustdt/ide/ui/editor/RustEditor.java
+++ plugin_ide.ui/src/com/github/rustdt/ide/ui/editor/RustEditor.java
@@ -13,9 +13,14 @@ package com.github.rustdt.ide.ui.editor;
 import melnorme.lang.ide.ui.EditorSettings_Actual;
 import melnorme.lang.ide.ui.LangUIPlugin;
 import melnorme.lang.ide.ui.editor.structure.AbstractLangStructureEditor;
+import melnorme.lang.ide.ui.editor.structure.LangOutlinePage;
+import melnorme.lang.ide.ui.editor.structure.StructureElementContentProvider;
 import melnorme.lang.ide.ui.editor.text.LangPairMatcher;
 import melnorme.lang.ide.ui.text.AbstractLangSourceViewerConfiguration;
 import melnorme.lang.ide.ui.text.LangSourceViewerConfiguration;
+import melnorme.lang.tooling.structure.IStructureElementContainer;
+import melnorme.lang.tooling.structure.StructureElement;
+import melnorme.lang.tooling.structure.StructureElementKind;
 import melnorme.util.swt.jface.text.ColorManager2;
 
 public class RustEditor extends AbstractLangStructureEditor {
@@ -30,6 +35,35 @@ public class RustEditor extends AbstractLangStructureEditor {
 		ColorManager2 colorManager = LangUIPlugin.getInstance().getColorManager();
 		return new LangSourceViewerConfiguration(getPreferenceStore(), colorManager, this, 
 			EditorSettings_Actual.getStylingPreferences());
+	}
+	
+	@Override
+	protected LangOutlinePage init_createOutlinePage() {
+		return new LangOutlinePage(this) {
+			@Override
+			protected StructureElementContentProvider createContentProvider() {
+				return new SpecializedStructureElementContentProvider();
+			}
+		};
+	}
+	
+	public class SpecializedStructureElementContentProvider extends StructureElementContentProvider {
+		@Override
+		protected Object[] getChildren(IStructureElementContainer elementContainer) {
+			if(elementContainer instanceof StructureElement) {
+				StructureElement structureElement = (StructureElement) elementContainer;
+				
+				if(structureElement.getKind() == StructureElementKind.ENUM) {
+					return null; // don't provide enum variants 
+				}
+				
+				if(structureElement.getKind() == StructureElementKind.FUNCTION
+						|| structureElement.getKind() == StructureElementKind.USE_GROUP) {
+					return null; // don't provide children, simplify structure 
+				}
+			}
+			return super.getChildren(elementContainer);
+		}
 	}
 	
 }
