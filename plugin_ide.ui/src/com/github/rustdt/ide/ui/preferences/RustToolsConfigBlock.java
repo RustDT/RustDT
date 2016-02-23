eb568bbe5167ec78d812036e410b7eae802d7f49
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-02-23 17:43:43 +0000

    Final round of refactoring, fixed widget status when disabled.

--- plugin_ide.ui/src/com/github/rustdt/ide/ui/preferences/RustToolsConfigBlock.java
+++ plugin_ide.ui/src/com/github/rustdt/ide/ui/preferences/RustToolsConfigBlock.java
@@ -21,7 +21,6 @@ import melnorme.lang.ide.ui.preferences.LangSDKConfigBlock;
 import melnorme.lang.ide.ui.preferences.common.PreferencesPageContext;
 import melnorme.lang.ide.ui.preferences.pages.DownloadToolTextField;
 import melnorme.lang.ide.ui.utils.operations.BasicUIOperation;
-import melnorme.util.swt.components.IDisableableWidget;
 import melnorme.util.swt.components.fields.ButtonTextField;
 import melnorme.util.swt.components.fields.CheckBoxField;
 import melnorme.util.swt.components.fields.DirectoryTextField;
@@ -29,15 +28,12 @@ import melnorme.utilbox.collections.Indexable;
 
 public class RustToolsConfigBlock extends LangSDKConfigBlock {
 	
-	protected final ButtonTextField sdkSrcLocation = new DirectoryTextField("Rust 'src' Directory:");
 	protected final RacerLocationGroup racerGroup = new RacerLocationGroup(); 
 	protected final RainicornLocationGroup rainicornGroup = new RainicornLocationGroup();
 	protected final RustFmtLocationGroup rustfmtGroup = new RustFmtLocationGroup();
 	
 	public RustToolsConfigBlock(PreferencesPageContext prefContext) {
 		super(prefContext);
-		
-		bindToDerivedPreference(sdkSrcLocation, RustSDKPreferences.SDK_SRC_PATH3);
 		
 		addSubComponent(racerGroup);
 		addSubComponent(rainicornGroup);
@@ -47,9 +43,12 @@ public class RustToolsConfigBlock extends LangSDKConfigBlock {
 	@Override
 	protected LanguageSDKLocationGroup init_createSDKLocationGroup() {
 		return new LanguageSDKLocationGroup() {
-			@Override
-			protected Indexable<IDisableableWidget> getSubWidgets() {
-				return super.getSubWidgets().toArrayList().addElements(sdkSrcLocation);
+			
+			protected final ButtonTextField sdkSrcLocation = new DirectoryTextField("Rust 'src' Directory:");
+			
+			{
+				addSubComponent(sdkSrcLocation);
+				bindToDerivedPreference(sdkSrcLocation, RustSDKPreferences.SDK_SRC_PATH3);
 			}
 		};
 	}
@@ -69,7 +68,8 @@ public class RustToolsConfigBlock extends LangSDKConfigBlock {
 			bindToDerivedPreference(this.toolLocationField, RustSDKPreferences.RACER_PATH);
 			CheckBoxField showErrorsDialogOption = new CheckBoxField(
 				"Show error dialog if " + toolName + " failures occur.");
-			subwidgets.add(showErrorsDialogOption);
+			
+			this.addSubComponent(showErrorsDialogOption);
 			prefContext.bindToPreference(showErrorsDialogOption, 
 				ContentAssistPreferences.ShowDialogIfContentAssistErrors);
 		}
@@ -101,11 +101,6 @@ public class RustToolsConfigBlock extends LangSDKConfigBlock {
 		}
 		
 		@Override
-		protected Indexable<IDisableableWidget> getSubWidgets() {
-			return list(toolLocationField);
-		}
-		
-		@Override
 		protected BasicUIOperation do_getDownloadButtonHandler(DownloadToolTextField downloadToolTextField) {
 			return new Start_CargoInstallJob_Operation(toolName, downloadToolTextField,
 				RAINICORN_CargoInstallArgs,
@@ -131,12 +126,9 @@ public class RustToolsConfigBlock extends LangSDKConfigBlock {
 			super("rustfmt");
 			
 			bindToDerivedPreference(this.toolLocationField, RustSDKPreferences.RUSTFMT_PATH);
+			
+			this.addSubComponent(formatOnSaveField);
 			prefContext.bindToPreference(this.formatOnSaveField, RustSDKPreferences.FORMAT_ON_SAVE);
-		}
-		
-		@Override
-		protected Indexable<IDisableableWidget> getSubWidgets() {
-			return list(toolLocationField, formatOnSaveField);
 		}
 		
 		@Override
