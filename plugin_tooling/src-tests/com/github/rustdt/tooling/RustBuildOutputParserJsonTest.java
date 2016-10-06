f75e9baf09ac380ae0cc8a2b8b16039d7d604661
Pieter Penninckx <Pieter.Penninckx@scarlet.be>
2016-10-06 09:40:25 +0200

    Move inline Json in tests from to separate json files.

--- plugin_tooling/src-tests/com/github/rustdt/tooling/RustBuildOutputParserJsonTest.java
+++ plugin_tooling/src-tests/com/github/rustdt/tooling/RustBuildOutputParserJsonTest.java
@@ -52,117 +52,28 @@ public class RustBuildOutputParserJsonTest extends RustBuildOutputParserTest {
 		}
 		
 		testParseMessages(buildParser,
-			"{\"message\":\"BLAH BLAH BLAH\",\"level\":\"warning\"," + 
-				"\"spans\":[" +
-					"{"+
-						"\"file_name\":\"src/main.rs\",\"byte_start\":0,\"byte_end\":10,\"line_start\":1," + 
-						"\"line_end\":3,\"column_start\":2,\"column_end\":17,\"is_primary\":true," + 
-						"\"label\":null,\"suggested_replacement\":null,"+
-						"\"expansion\":null"+
-					"}"+
-				"],"+
-				"\"children\":[],\"rendered\":null" +
-			"}",
+			getClassResource("simple_warning.json"),
 			listFrom(msg(path("src/main.rs"), 1, 2, 3, 17, WARNING, "BLAH BLAH BLAH")));
 		
 		testParseMessages(buildParser,
-				"{\"message\":\"BLAH BLAH BLAH\",\"level\":\"warning\"," + 
-					"\"spans\":["+
-						"{"+
-							"\"file_name\":\"src/main.rs\",\"byte_start\":0,\"byte_end\":10,\"line_start\":1," + 
-							"\"line_end\":3,\"column_start\":2,\"column_end\":17,\"is_primary\":true," + 
-							"\"text\":["+
-								"{\"text\":\"fn a() {\",\"highlight_start\":1,\"highlight_end\":9},"+
-								"{\"text\":\"}\",\"highlight_start\":1,\"highlight_end\":2}"+
-							"],"+
-							"\"label\":null,\"suggested_replacement\":null,\"expansion\":null"+
-						"}"+
-					"]," +
-					"\"children\":[],\"rendered\":null"+
-				"}\n" +  
-				"{\"message\":\"XXX\",\"code\":null,\"level\":\"error\"," + 
-					"\"spans\":["+
-						"{"+
-							"\"file_name\":\"src/main.rs\",\"byte_start\":47,\"byte_end\":48,\"line_start\":2,"+
-							"\"line_end\":2,\"column_start\":22,\"column_end\":23,\"is_primary\":true,"+
-							"\"text\":["+
-								"{\"text\":\"const STRING2: str = ;\",\"highlight_start\":22,\"highlight_end\":23}" +
-							"],"+
-							"\"label\":null,\"suggested_replacement\":null,\"expansion\":null"+
-						"}"+
-					"],"+
-					"\"children\":[],\"rendered\":null"+
-				"}",
+				getClassResource("simple_warning_and_error.json"),
 				listFrom(msg(path("src/main.rs"), 1, 2, 3, 17, WARNING, "BLAH BLAH BLAH"),
 						 msg(path("src/main.rs"), 2, 22, 2, 23, ERROR, "XXX")));
 		
 		testParseMessages(buildParser, 
-			"{\"message\":\"mismatched types\","+
-				"\"code\":{"+
-					"\"code\":\"E0308\","+
-					"\"explanation\":\"EXPLENATION\""+
-				"}," + 
-				"\"level\":\"error\"," + 
-				"\"spans\":["+
-					"{"+
-						"\"file_name\":\"src/main.rs\",\"byte_start\":40,\"byte_end\":41," + 
-						"\"line_start\":6,\"line_end\":6,\"column_start\":9,\"column_end\":10,\"is_primary\":true," + 
-						"\"text\":[{\"text\":\"\\treturn 0;\",\"highlight_start\":9,\"highlight_end\":10}]," + 
-						"\"label\":\"expected (), found integral variable\",\"suggested_replacement\":null,"+
-						"\"expansion\":null"+
-					"}"+
-				"]," +
-				"\"children\":["+
-					"{"+
-						"\"message\":\"expected type `()`\",\"code\":null,\"level\":\"note\","+
-						"\"spans\":[],\"children\":[],\"rendered\":null"+
-					"},{"+
-						"\"message\":\" found type `{integer}`\",\"code\":null,\"level\":\"note\","+
-						"\"spans\":[],\"children\":[],\"rendered\":null"+
-					"}"+
-				"]," + 
-				"\"rendered\":null"+
-			"}\n" + 
-			"{"+
-				"\"message\":\"aborting due to 2 previous errors\",\"code\":null,\"level\":\"error\","+
-				"\"spans\":[],\"children\":[],\"rendered\":null"+
-			"}\n", 
+			getClassResource("error_with_children.json"), 
 			listFrom(msg(path("src/main.rs"), 6, 9, 6, 10, ERROR, 
 						"mismatched types [E0308]: "+ // message + [Error code]
 						"expected (), found integral variable " + // Label from span 
 						"(expected type `()`,  found type `{integer}`)"))); // (message child 1, message child 2)
 		
 		testParseMessages(buildParser,
-			"{\"message\":\"aborting due to previous error\",\"code\":null,\"level\":\"error\",\"spans\":[],"+
-			"\"children\":[],\"rendered\":null}",
+			getClassResource("aborting_due_to_error.json"),
 			listFrom());
 		
 		
 		testParseMessages(buildParser,
-			"{\"message\":\"cannot borrow `i` as mutable more than once at a time\"," + 
-			"\"code\":{\"code\":\"E0499\"," + 
-				"\"explanation\":\"EXPLENATION\"},"+
-			"\"level\":\"error\",\"spans\":["+
-				"{"+
-					"\"file_name\":\"test.rs\",\"byte_start\":74,\"byte_end\":75,\"line_start\":7,\"line_end\":7,"+
-					"\"column_start\":15,\"column_end\":16,\"is_primary\":false,"+
-					"\"text\":[{\"text\":\"\\tlet j = &mut i;\",\"highlight_start\":15,\"highlight_end\":16}],"+
-					"\"label\":\"first mutable borrow occurs here\","+
-					"\"suggested_replacement\":null,\"expansion\":null"+
-				"},{" + 
-					"\"file_name\":\"test.rs\",\"byte_start\":91,\"byte_end\":92,\"line_start\":8,\"line_end\":8,"+
-					"\"column_start\":15,\"column_end\":16,\"is_primary\":true,"+
-					"\"text\":[{\"text\":\"\\tlet k = &mut i;\",\"highlight_start\":15,\"highlight_end\":16}],"+
-					"\"label\":\"second mutable borrow occurs here\","+
-					"\"suggested_replacement\":null,\"expansion\":null"+
-				"},{"+
-					"\"file_name\":\"test.rs\",\"byte_start\":94,\"byte_end\":95,\"line_start\":9,\"line_end\":9,"+
-					"\"column_start\":1,\"column_end\":2,\"is_primary\":false,"+
-					"\"text\":[{\"text\":\"}\",\"highlight_start\":1,\"highlight_end\":2}],"+
-					"\"label\":\"first borrow ends here\",\"suggested_replacement\":null,\"expansion\":null"+
-				"}"+
-			"]," + 
-			"\"children\":[],\"rendered\":null}",
+			getClassResource("error_with_multiple_spans.json"),
 			listFrom(msg(path("test.rs"), 7, 15, 7, 16, INFO, // First span. Not primary, so INFO
 							"cannot borrow `i` as mutable more than once at a time: " + // message
 							"first mutable borrow occurs here"), // Label from first span
@@ -174,21 +85,7 @@ public class RustBuildOutputParserJsonTest extends RustBuildOutputParserTest {
 							"first borrow ends here"))); // Label from the third span.
 		
 		testParseMessages(buildParser,
-			"{\"message\":\"the trait bound `u8: Tr` is not satisfied\"," +
-				"\"code\":{\"code\":\"E0277\",\"explanation\":\"EXPLENATION\"}," +
-				"\"level\":\"error\"," +
-				"\"spans\":[" +
-					"{" +
-						"\"file_name\":\"test.rs\",\"byte_start\":329,\"byte_end\":330,\"line_start\":39," +
-						"\"line_end\":39,\"column_start\":2,\"column_end\":3,\"is_primary\":true," +
-						"\"text\":[{\"text\":\"\\tx(0_u8);\",\"highlight_start\":2,\"highlight_end\":3}]," +
-						"\"label\":\"trait `u8: Tr` not satisfied\",\"suggested_replacement\":null,\"expansion\":null" +
-					"}" + 
-				"]," + 
-				"\"children\":[{" +
-						"\"message\":\"required by `x`\",\"code\":null,\"level\":\"note\"," +
-						"\"spans\":[],\"children\":[],\"rendered\":null" +
-				"}],\"rendered\":null}",
+			getClassResource("error_with_single_child.json"),
 			listFrom(msg(path("test.rs"), 39, 2, 39, 3, ERROR, // Primary span, so copy the severity (ERROR)
 						"the trait bound `u8: Tr` is not satisfied [E0277]: "+ // message + [error code] 
 						"trait `u8: Tr` not satisfied " + // Label of the span
@@ -198,85 +95,11 @@ public class RustBuildOutputParserJsonTest extends RustBuildOutputParserTest {
 		// If the main function is not found, we naively assume that it is in the `src/main.rs` file.
 		// TODO: find out in which file the compiler looks for the main function.
 		testParseMessages(buildParser,
-			"{" +
-				"\"message\":\"main function not found\",\"code\":null,\"level\":\"error\"," +
-				"\"spans\":[],\"children\":[],\"rendered\":null" +
-			"}",
+			getClassResource("main_function_not_found.json"),
 			listFrom(msg(path("src/main.rs"), 1, 1, 1, 1, ERROR, "main function not found")));
 		
 		testParseMessages(buildParser,
-			"{\"message\":\"mismatched types\","+
-				"\"code\":{\"code\":\"E0308\","+
-					"\"explanation\":\"EXPLENATION\""+
-				"},"+
-				"\"level\":\"error\","+
-				"\"spans\":["+
-					"{"+
-						"\"file_name\":\"<std macros>\",\"byte_start\":23234,\"byte_end\":23245,\"line_start\":5,"+
-						"\"line_end\":5,\"column_start\":22,\"column_end\":33,\"is_primary\":true,"+
-						"\"text\":[{\"text\":\"if ! ( * left_val == * right_val ) {\","+
-							"\"highlight_start\":22,\"highlight_end\":33}"+
-						"],"+
-						"\"label\":\"expected enum `std::option::Option`, found &{integer}\","+
-						"\"suggested_replacement\":null,"+
-						"\"expansion\":{"+
-							"\"span\":{"+
-								"\"file_name\":\"src/test.rs\",\"byte_start\":20203,\"byte_end\":20234,"+
-								"\"line_start\":331,\"line_end\":331,\"column_start\":2,\"column_end\":33,"+
-								"\"is_primary\":false,"+
-								"\"text\":[{"+
-									"\"text\":\"\\tassert_eq!(Some(&3_usize), &3);\","+
-									"\"highlight_start\":2,\"highlight_end\":33"+
-								"}],"+
-								"\"label\":null,\"suggested_replacement\":null,\"expansion\":null"+
-							"},"+
-							"\"macro_decl_name\":\"assert_eq!\","+
-							"\"def_site_span\":{"+
-								"\"file_name\":\"<std macros>\",\"byte_start\":23108,\"byte_end\":23746,"+
-								"\"line_start\":1,\"line_end\":18,\"column_start\":1,\"column_end\":71,"+
-								"\"is_primary\":false,"+
-								"\"text\":[{\"text\":\"( $ left : expr , $ right : expr ) => (\","+
-									"\"highlight_start\":1,\"highlight_end\":40},{\"text\":\"{\",\"highlight_start\":"+
-									"1,\"highlight_end\":2},{\"text\":\"match ( & $ left , & $ right ) {\","+
-									"\"highlight_start\":1,\"highlight_end\":33},{\"text\":\"( left_val , right_val )"+
-									"=> {\",\"highlight_start\":1,\"highlight_end\":30},{\"text\":\"if ! ( * left_val"+
-									" == * right_val ) {\",\"highlight_start\":1,\"highlight_end\":37},{\"text\""+
-									":\"panic ! (\",\"highlight_start\":1,\"highlight_end\":10},{\"text\":\"\\"+
-									"\"assertion failed: `(left == right)` \\\\\",\"highlight_start\":1,"+
-									"\"highlight_end\":39},{\"text\":\"                           (left: `{:?}`,"+
-									" right: `{:?}`)\\\"\",\"highlight_start\":1,\"highlight_end\":58},"+
-									"{\"text\":\", left_val , right_val ) } } } } ) ; (\",\"highlight_start\":"+
-									"1,\"highlight_end\":39},{\"text\":\"$ left : expr , $ right : expr , $ ( $ "+
-									"arg : tt ) * ) => (\",\"highlight_start\":1,\"highlight_end\":59},"+
-									"{\"text\":\"{\",\"highlight_start\":1,\"highlight_end\":2},"+
-									"{\"text\":\"match ( & ( $ left ) , & ( $ right ) ) {\",\"highlight_start"+
-									"\":1,\"highlight_end\":41},{\"text\":\"( left_val , right_val ) => {\","+
-									"\"highlight_start\":1,\"highlight_end\":30},{\"text\":\"if ! ( * left_val "+
-									"== * right_val ) {\",\"highlight_start\":1,\"highlight_end\":37},{\"text\":"+
-									"\"panic ! (\",\"highlight_start\":1,\"highlight_end\":10},{\"text\""+
-									":\"\\\"assertion failed: `(left == right)` \\\\\",\"highlight_start\":1,"+
-									"\"highlight_end\":39},{\"text\":\"                           (left: `{:?}`, "+
-									"right: `{:?}`): {}\\\"\",\"highlight_start\":1,\"highlight_end\":62},"+
-									"{\"text\":\", left_val , right_val , format_args ! ( $ ( $ arg ) * ) ) } } } } "+
-									") ;\",\"highlight_start\":1,\"highlight_end\":71}],\"label\":null,\"suggested_"+
-									"replacement\":null,\"expansion\":null"+
-								"}"+
-							"}"+
-						"}"+
-					"],"+
-					"\"children\":["+
-						"{"+
-							"\"message\":\"expected type `std::option::Option<&usize>`\","+
-							"\"code\":null,"+
-							"\"level\":\"note\",\"spans\":[],\"children\":[],\"rendered\":null"+
-						"},{"+
-							"\"message\":\"   found type `&{integer}`\","+
-							"\"code\":null,"+
-							"\"level\":\"note\",\"spans\":[],\"children\":[],\"rendered\":null"+
-						"}"+
-					"],"+
-				"\"rendered\":null"+
-			"}",
+			getClassResource("macro_error.json"),
 			listFrom(msg(path("<std macros>"), 5, 22, 5, 33, ERROR, "mismatched types [E0308]: "+
 					"expected enum `std::option::Option`, found &{integer} "+
 					"("+
